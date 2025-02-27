2e00532cea83e46405c2c51d0db5112c9b09311c
felix <felix@Jaschul.de>
2025-02-27 10:38:27 +0100

    27.02.2025

--- src/Q2/Adts/Schlange/Schlange.java
+++ src/Q2/Adts/Schlange/Schlange.java
@@ -1,4 +1,61 @@
 package src.Q2.Adts.Schlange;
 
 public class Schlange {
-}
+
+    int groesse;
+    Knoten head;
+    Knoten tail;
+
+    private class Knoten {
+
+        int value;
+        Knoten next;
+
+        public Knoten(int value) {
+
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public Schlange() {
+
+        this.head = null;
+        this.tail = null;
+        this.groesse = 0;
+    }
+
+    private static void main(String[] args) {
+
+        Schlange schlange = new Schlange();
+    }
+
+    public boolean isEmpty() {
+
+        return groesse == 0;
+    }
+
+    public void enqueue(int value) {
+
+        Knoten knoten = new Knoten(value);
+
+        if (this.isEmpty()) {
+
+            head = knoten;
+            tail = knoten;
+        } else {
+
+            tail.next = knoten;
+            tail = knoten;
+        }
+
+        groesse++;
+    }
+
+    public void dequeue() {
+
+        if (this.isEmpty()) {
+
+
+        }
+    }
