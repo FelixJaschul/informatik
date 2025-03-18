b938748761c438486e90d39bdbe694d839cb9e1f
felix <Felix@Jaschul.de>
2025-03-18 12:44:41 +0100

    18.03.2025

--- src/Q2/Sortieralgorithmen/RadixSort.java
+++ src/Q2/Sortieralgorithmen/RadixSort.java
@@ -46,14 +46,12 @@ class RadixSort implements Sort {
     // Der Counting-Sort ist ein wichtiger Bestandteil des RadixSort
     private static void countingSort(int[] arr, int exp) {
         int n = arr.length;
-        int[] output = new int[n]; // Ergebnisarray
-        int[] count = new int[10]; // 0-9 für Dezimalziffern
-        // Zähle die Häufigkeit jeder Ziffer
+        int[] output = new int[n];
+        int[] count = new int[10];
         for (int i = 0; i < n; i++) {
             int digit = (arr[i] / exp) % 10;
             count[digit]++;
         }
-        // Verändere count[i], damit es die Position im Output-Array enthält
         for (int i = 1; i < 10; i++) count[i] += count[i - 1];
         // Baue das sortierte Array von hinten nach vorne (stabil)
         for (int i = n - 1; i >= 0; i--) {
@@ -63,7 +61,7 @@ class RadixSort implements Sort {
             SortierUtils.incSwapCount();
         }
         // Kopiere das sortierte Array zurück ins Original-Array
-        // Ich benutze nicht copyArray aus SortierUtils, da ich sonst den SwapCount nicht zählen könnte
+        // Ich benutze hier nicht copyArray aus SortierUtils, da ich sonst den SwapCount nicht zählen könnte
         for (int i = 0; i < n; i++) {
             if (arr[i] != output[i]) {
                 arr[i] = output[i];
