413c5885de83b73ad9551012ec45fedb1e704054
felixjaschul <Felix@Jaschul.de>
2025-10-06 12:17:54 +0200

    06.10.2025

--- python/src/Funktional/Guards/Aufgabe4.py
+++ python/src/Funktional/Guards/Aufgabe4.py
@@ -7,10 +7,12 @@ def multiAll(t: tuple, n: int) -> tuple:
     return tuple(map(lambda x: x * n, t))
 
 def versetzen(t: tuple) -> tuple | str:
-    if t[2] < 12: return t[0], t[1] + 1
+    if t[2] < 12: return t[0], t[1], t[2] + 1 , t[3]
     else: return "Abschluss"
 
 def name(t): return t[0]
 def vorname(t): return t[1]
 def jahrgang(t): return t[2]
 def zug(t): return t[3]
+
+print(versetzen(("A", "B", 11, 1)))
